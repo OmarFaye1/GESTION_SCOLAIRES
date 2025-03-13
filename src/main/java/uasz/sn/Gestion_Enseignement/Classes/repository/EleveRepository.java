0ea7ac95e721ec15c0bc8d0ea05b2148c4011ac7
sattoutall <satoutall2002@gmail.com>
2025-03-13 16:32:53 +0000

    modification suppression d'une classe

--- src/main/java/uasz/sn/Gestion_Enseignement/Classes/repository/EleveRepository.java
+++ src/main/java/uasz/sn/Gestion_Enseignement/Classes/repository/EleveRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface EleveRepository extends JpaRepository<Eleve, Long> {
     // Méthode pour récupérer les élèves d'une classe spécifique
     List<Eleve> findByClasse(Classe classe);
+    // Ajouter la méthode pour vérifier si un username existe
+    boolean existsByUsername(String username);
 }
